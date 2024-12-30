bd305bf962ee42d83d058cabe48010b7f492a1c6
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-12-30 15:34:13 -0800

    Create 2.10.py

---
+++ Programming/2.10.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+A = int(input("The mass number A is:",)
+Z = int(input("The atomic number Z is:",)
+#    
+if A%2 !== 0:
+  a5 = 0
+elsif (A+Z)%2 == 0
+  a5 = float(12.0)
+else  
+  a5 = float(-12.0)
+#
+a1 = float(15.8)
+a2 = float(18.3)
+a3 = float(0.714)
+a4 - float(23.2)
+#
+B = a1*A - a2*A**(2/3) - a3*(Z**2)/(A**(1/3)) - a4*(A - 2*Z)**2/A + a5/A**(1/2)
+
+
+
+
