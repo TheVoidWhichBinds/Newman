141a98d33769315d362aa247a5669451e3c6a513
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-12-30 23:59:07 +0000

    2.10

--- Programming/2.10.py
+++ Programming/2.10.py
@@ -1,22 +1,37 @@
-import numpy as np
+from numpy import empty, size
 
-A = int(input("The mass number A is:",)
-Z = int(input("The atomic number Z is:",)
+# a)
+
+A = int(input("The mass number A is: "))
+Z = int(input("The atomic number Z is: "))
 #    
-if A%2 !== 0:
+if A%2 != 0:
   a5 = 0
-elsif (A+Z)%2 == 0
+elif (A+Z)%2 == 0:
   a5 = float(12.0)
-else  
+else:
   a5 = float(-12.0)
 #
 a1 = float(15.8)
 a2 = float(18.3)
 a3 = float(0.714)
-a4 - float(23.2)
+a4 = float(23.2)
 #
 B = a1*A - a2*A**(2/3) - a3*(Z**2)/(A**(1/3)) - a4*(A - 2*Z)**2/A + a5/A**(1/2)
 
+print(f"The binding energy of this atom is {B} MeV")
 
+
+# c)
+
+Z = int(input("The atomic number Z is: "))
+def stable_atom(Z):
+  #
+  E_per_nucleon = empty(size(range(Z,3*Z)))
+  for A,i in range(Z, 3*Z):
+    B = a1*A - a2*A**(2/3) - a3*(Z**2)/(A**(1/3)) - a4*(A - 2*Z)**2/A + a5/A**(1/2)
+    E_per_nucleon[i] = B/A
+  #
+  A_stable  
 
 
