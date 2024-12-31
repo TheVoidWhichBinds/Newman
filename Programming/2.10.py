cd3bc45f0ad715b061de82cf5a79a961f513400d
TheVoidWhichBinds <daviizarshenas@gmail.com>
2024-12-31 00:33:19 +0000

    generalizing functions

--- Programming/2.10.py
+++ Programming/2.10.py
@@ -1,28 +1,36 @@
 import numpy as np
 
-# a)
-
-A = int(input("The mass number A is: "))
-Z = int(input("The atomic number Z is: "))
-#    
-if A%2 != 0:
-  a5 = 0
-elif (A+Z)%2 == 0:
-  a5 = float(12.0)
-else:
-  a5 = float(-12.0)
-#
+#constants:
 a1 = float(15.8)
 a2 = float(18.3)
 a3 = float(0.714)
 a4 = float(23.2)
-#
-def binding_energy(A):
+
+#a5 function:
+def a_5(A,Z):
+  if A%2 != 0:
+    a5 = 0
+  elif (A+Z)%2 == 0:
+    a5 = float(12.0)
+  else:
+    a5 = float(-12.0)
+  return a5
+
+
+
+# a)
+
+A = int(input("The mass number A is: "))
+Z = int(input("The atomic number Z is: "))
+
+def binding_energy(A,Z):
+  a5 = a_5(A,Z)
   B = a1*A - a2*A**(2/3) - a3*(Z**2)/(A**(1/3)) - a4*(A - 2*Z)**2/A + a5/A**(1/2)
   return B
 
-B = binding_energy(A)
+B = binding_energy(A,Z)
 print(f"The binding energy of this atom is {B} MeV")
+
 
 
 # c)
@@ -30,8 +38,8 @@ print(f"The binding energy of this atom is {B} MeV")
 Z = int(input("The atomic number Z is: "))
 def stable_atom(Z):
   #
-  A = np.arange(Z,3Z)
-  B = binding_energy(A)
+  A = np.arange(Z,3*Z)
+  B = binding_energy(A,Z)
   index_stable = np.argmax(B/A)
   A_stable = A[index_stable]
   return A_stable
