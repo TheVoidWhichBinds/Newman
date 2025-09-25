ab70798b63099f3466ef7c25b2a6902e7205ebab
TheVoidWhichBinds <daviizarshenas@gmail.com>
2025-09-25 03:57:39 +0000

    Relativistic Electron Momentum. Curve looks right but one of the constants must be off because it's too steep

--- Scratch/REM.py
+++ Scratch/REM.py
@@ -1,32 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 
+R_0   = 1.0E-24   #4*pi*r_0^2[cm^2]
+E_r   = 0.511     #m_0*c^2   [MeV]
+n     = 3.88E20   #NZ        [#/cm^3]
+I     = 86E-6     #          [MeV]
+E_max = 1.064     #          [MeV]
+s_min = np.pi*1.9 #          [cm]
+s_max = np.pi*3.1 #          [cm]
 
-
-R_0 = 1.0E-24 #4*pi*r_0^2 [cm^2]
-E_r = 0.511 #m_0*c^2 [MeV]
-n = 3.88E20 #NZ [#/cm^3]
-I = 86E-6 #[MeV]
-E = 1.064 #[MeV]
-
-eps = 0.1 #[MeV]
-E_range = np.arange(E - eps, 0.01)
 
 def Beta_func(E):
-  Beta = np.sqrt(1 - (E_r/E)**2)
-  return Beta
+    Beta = np.sqrt(1 - (E_r/E)**2)
+    return Beta
 
-def E_loss_func(E):
-  Beta = Beta_func(E)
-  E_loss = R_0*E_r*n/Beta * ( np.ln(Beta*((E+E_r)/I)*np.sqrt(E/E_r)) - (Beta**2)/2 )
-  return E_loss
+def dE_ds(s,E):
+    b = Beta_func(E)
+    return (R_0*E_r*n/(b*b) * np.log(b*((E+E_r)/I)*np.sqrt(E/E_r)) - 0.5*b*b)
 
-E_loss = E_loss_func(E)
+E_loss = solve_ivp(
+    dE_ds, 
+    t_span=(0, 2), 
+    y0=[E_max], 
+    t_eval = np.linspace(0, 2, 200))
 
 
 plt.figure()
-plt.xlabel("Total Relativistic Energy E [MeV]")
-plt.ylabel("Energy loss per distance [MeV/cm]")
-plt.title("Relativistic Energy Loss")
-plt.plot(E_range, E_loss)
-plt.savefig("Relativistic_E_Loss")
+plt.plot(E_loss.t, E_loss.y[0])
+plt.xlabel("Path length s [cm]")
+plt.ylabel("Total Relativistic Energy E(s) [MeV]")
+plt.title("Energy vs Distance")
+plt.savefig("Energy_Loss")
