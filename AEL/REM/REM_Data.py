52c5469e41abbf50e2f4e390da2052d99116dacf
Davii Zarshenas <daviizarshenas@gmail.com>
2025-10-07 16:52:18 -0400

    Data Reader REM

--- AEL/REM/REM_Data.py
+++ AEL/REM/REM_Data.py
@@ -1,35 +1,61 @@
 import matplotlib.pyplot as plt
 import numpy as np 
+import pandas as pd
+import os
+downloads_dir = os.path.expanduser('~/Downloads') 
 
 
-num_fixed_B = 25  #number of data points collected at a fixed magnetic field
-num_B_val = 28  #number of different magnetic field values scanned over
-B_offset = 500 #B field data saved in .txt with fixed offset 
+#-------- data averaging function ----------------------------#
+def data_averaging(data, knob_pos, upper_B, lower_B, B_offset):
+    #compute B-field from Gaussmeter reading:
+    dial_rate = (upper_B - lower_B) / knob_pos
+    data[:, 4] = data[:, 1] + B_offset + dial_rate * data[:, 0]
 
-data = np.loadtxt("REM_test.txt", delimiter=",") 
-data_avg = np.empty((num_B_val, 4))
+    #group data by knob position (column 0) and average the other columns:
+    #(for some reason, # data pts per knob pos inconsistent)
+    col0 = data[:, 0]
+    change = np.concatenate(([True], col0[1:] != col0[:-1], [True])) #boolean array where knobob position changes
+    bounds = np.flatnonzero(change) #indexing where knobob position changes
+
+    #averaging data in each group:
+    out = []
+    for s, e in zip(bounds[:-1], bounds[1:]): #grouping indices of same knob position
+        rows = data[s:e, :] #all rows with same knob position
+        out.append(np.mean(rows, axis=0)) #average all columns in those rows
+    data_avg = np.vstack(out) #stacking all averaged rows into array
+
+    return data_avg
 
 
-for i in range(num_B_val):
-    rows = data[i*num_fixed_B:(i+1)*num_fixed_B, :] #slicing rows i*num_fixed_B : (i+1)*num_fixed_B, all columns
-    data_avg[i, :] = np.mean(rows, axis=0) #averaging over rows
+
+#--------- parameters for averaging function -------------------#
+#knob_pos = # knob positions moved through (# individual B values)         
+#upper_B  = max Gaussmeter reading from screen display    
+#lower_B  = initial Gaussmeter reading from screen display 
+#B_offset = Gaussmeter reading difference between screen and Arduino input 
 
 
-#[0] column = index
-B_field = data_avg[:,1] + B_offset #[1] column = B field [Gauss]
-#[2] column = null
-pulses = data_avg[:, 3] #[3] column = pulse count
+
+#--------- load data, calling function ------------------------------#
+data1 = pd.read_excel("Bi848.xlsx", sheet_name=0, header=0).to_numpy()
+data2 = pd.read_excel("Bi1146.xlsx", sheet_name=0, header=0).to_numpy()
+#data3 = pd.read_excel("Arduino_Data.xlsx", sheet_name=2, header=0).to_numpy()
+
+data_avg = np.vstack(
+    [data_averaging(data1, 298, 1146, 848, 668),
+     data_averaging(data2, 700, 1568, 1146, 908), 
+     #data_averaging(data3, knob_pos, upper_B, lower_B, B_offset)]
+])
+B_field = data_avg[:, 4]
+pulses  = data_avg[:, 3]
 
 
+
+#--------- plotting averaged data ----------#
 plt.figure()
 plt.title("Relativistic Electron Momentum")
 plt.xlabel("Average Magnetic Field (Gauss)")
 plt.ylabel("Average Electron Count")
-plt.scatter(B_field, pulses, marker='o', color = 'purple')
-plt.savefig("REM.png", dpi = 200)
-
-
-
-
-
-
+plt.scatter(B_field, pulses, s=6, marker='o', color='purple')
+plt.savefig("REM.png", dpi=200)
+plt.savefig(os.path.join(downloads_dir, "REM.png"), dpi=200, bbox_inches="tight")
--- AEL/REM/Stopping_Power.py
+++ AEL/REM/Stopping_Power.py
@@ -1,24 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+import os
+downloads_dir = os.path.expanduser('~/Downloads') 
+
+#---------- constants ----------#
+E_r = 0.511 #m_0*c^2 [MeV]
+I = 86E-6 #[MeV]
+C = 9.234e-05 #0.5*rho*K*Z/A [MeV/cm]
+E_max = 1.064 #[MeV] initial energy of electron
+s_avg = np.pi*2.5 #[cm] average arc length to detector
+s_max = 50 #[cm] arbitrary large distance for energy loss plotting
 
 
-E_r = 0.511 # m_0*c^2 [MeV]
-I = 86E-6 # [MeV]
-C = 9.234e-05 # 0.5*rho*K*Z/A [MeV/cm]
-E_max = 1.064 # [MeV]
-s_min = np.pi*1.9 # [cm]
-s_max = 50 # [cm]
-
-
-def beta_func(E):
+#---------- stopping power functions ----------#
+#classic relativistic beta (v/c) solved in terms of total energy E and rest energy E_r:
+def beta_func(E): 
     beta = np.sqrt(1 - (E_r/E)**2)
     return beta
 
-def gamma_func(E):
+#classic relativistic gamma (1/sqrt(1-beta^2)) solved in terms of total energy E and rest energy E_r
+def gamma_func(E): 
     gamma = E/E_r
     return gamma
 
+#stopping power function dE/ds in MeV/cm
 def stopping_rate(E):
     b = beta_func(E)
     g = gamma_func(E)
@@ -30,31 +36,42 @@ def stopping_rate(E):
     + 0.125*((g-1.0)/g)**2)
     return  E_deriv
 
-dE_ds = lambda s, E: -stopping_rate(E)
-E_loss = solve_ivp( 
+#---------- solving ODE for E as a function of distance traveled s ----------#
+dE_ds = lambda s, E: -stopping_rate(E) #puts stopping rate in form for ODE solver
+E_loss = solve_ivp( #ODE solver
     dE_ds, 
     t_span=(0, s_max), 
     y0=[E_max], 
     t_eval = np.linspace(0, s_max, 200))
 
 
-
-
-E_range = np.linspace(1.064, 10.0, 400) 
+#---------- plotting stopping rate [MeV/cm] ----------#
+E_range = np.linspace(0.7, 10.0, 400) #arbitrary range of energies for plotting
 S = stopping_rate(E_range)
-
-#### plotting stopping rate [MeV/cm] ####
 plt.figure()
 plt.plot(E_range, S)
 plt.xlabel("Energy [MeV]")
 plt.ylabel("Linear Stopping Power [MeV/cm]")
 plt.title("Relativistic Stopping Power")
 plt.savefig("Stopping_Power.png", dpi=200, bbox_inches="tight")
+plt.savefig(os.path.join(downloads_dir, "Stopping_Power.png"), dpi=200, bbox_inches="tight")
 
-#### plotting E as a func of distance traveled ####
+#---------- plotting E as a func of distance traveled -----------#
 plt.figure()
 plt.plot(E_loss.t, E_loss.y[0])
 plt.ylabel("Total Relativistic Energy [MeV]")
 plt.xlabel(" Distance [cm]")
 plt.title("Relativistic Electron Energy Loss")
 plt.savefig("Rel_Energy_Loss")
+plt.savefig(os.path.join(downloads_dir, "Rel_Energy_Loss.png"), dpi=200, bbox_inches="tight")
+
+#---------- finds E_det from trajectory arc length s ----------#
+#given E_loss plot x axis (distance s), and target distance s, find index of closest distance in s_range
+def detector_energy(s_range, s): 
+    i = np.searchsorted(s_range, s)
+    i = np.clip(i, 1, len(s_range)-1)
+    return i-1 if s - s_range[i-1] <= s_range[i] - s else i
+
+s_index = detector_energy(E_loss.t, s_avg) 
+E_det = E_loss.y[0][s_index] #energy at detector corresponding to average arc length s_avg
+print(f"Average energy at detector: {E_det:.3f} MeV")
