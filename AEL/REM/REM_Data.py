badfb7f087371c2b890d707d11dc0aee7ce12689
Davii Zarshenas <dz2596@nyu.edu>
2025-10-09 02:35:05 -0400

    final draft

--- AEL/REM/REM_Data.py
+++ AEL/REM/REM_Data.py
@@ -5,16 +5,16 @@ import os
 downloads_dir = os.path.expanduser('~/Downloads') 
 
 
+
 #-------- data averaging function ----------------------------#
 def data_averaging(data, knob_pos, upper_B, lower_B, B_offset):
     #compute B-field from Gaussmeter reading:
     dial_rate = (upper_B - lower_B) / knob_pos
     data[:, 4] = data[:, 1] + B_offset + dial_rate * data[:, 0]
-
     #group data by knob position (column 0) and average the other columns:
     #(for some reason, # data pts per knob pos inconsistent)
     col0 = data[:, 0]
-    change = np.concatenate(([True], col0[1:] != col0[:-1], [True])) #boolean array where knobob position changes
+    change = np.concatenate(([True], col0[1:] != col0[:-1])) #boolean array where knobob position changes
     bounds = np.flatnonzero(change) #indexing where knobob position changes
 
     #averaging data in each group:
@@ -23,11 +23,9 @@ def data_averaging(data, knob_pos, upper_B, lower_B, B_offset):
 
     for s, e in zip(bounds[:-1], bounds[1:]): #grouping indices of same knob position
         rows = data[s:e, :] #all rows with same knob position
-        #error analysis:
-        w_j = 1/(np.std(rows[:,4], axis=0))**2 #weights for avg B field standard error
-        w_list.append(w_j)
-        #averaging data columns over each knob position:
-        out.append(np.mean(rows, axis=0)) 
+        #error analysis & averaging:
+        out.append(np.mean(rows, axis=0))
+        w_list.append(1/np.std(rows[:, 4], ddof=1)**2) #weight for std error of B-field
     
     data_avg = np.vstack(out) #stacking all averaged rows into array
     w_array = np.array(w_list)
@@ -48,12 +46,14 @@ data1 = pd.read_excel("Bi848.xlsx", sheet_name=0, header=0).to_numpy()
 data2 = pd.read_excel("Bi1146.xlsx", sheet_name=0, header=0).to_numpy()
 data3 = pd.read_excel("Bi1568.xlsx", sheet_name=0, header=0).to_numpy()
 
+
+
 #--------- averaging data sets & generating std error of B-Field ---------#
 #inserting data runs from 848 Gauss to ???? Gauss
 avg_data = ([
      data_averaging(data1, 500, 1146, 848, 668),
      data_averaging(data2, 700, 1568, 1146, 908), 
-     data_averaging(data3, 400, 1822, 1568, 1250)
+     data_averaging(data3, 400, 1822, 1568, 1320)
 ])
 
 #extracting averaged B-field and pulse counts:
@@ -61,10 +61,18 @@ all_avg = np.vstack([out[0] for out in avg_data])
 B_field = all_avg[:, 4]
 pulses  = all_avg[:, 3]
 
+#finding B-field that results in peak counts:
+max_index = np.argmax(pulses)
+B_peak = B_field[max_index]
+print(f"Peak Pulses B-field: {B_peak:.3f} Gauss")
+
 #extracting error weights and calculating standard error of B-field:
 all_weights = np.concatenate([out[1] for out in avg_data])
 sigma_B = 1/np.sqrt(np.sum(all_weights, axis=0))
-print(f"Standard error of B-field: {sigma_B:.3f} Gauss")
+print(f"Std Error of B-field: {sigma_B:.3f} Gauss")
+
+
+
 
 #--------- plotting averaged data ----------#
 plt.figure()
