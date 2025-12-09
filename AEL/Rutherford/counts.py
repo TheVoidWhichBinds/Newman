b7391b7374026cb5c9a89d8bbf024e8770692a15
Davii Zarshenas <dz2596@nyu.edu>
2025-12-09 13:38:14 -0500

    final Rutherford

--- AEL/Rutherford/counts.py
+++ AEL/Rutherford/counts.py
@@ -24,10 +24,10 @@ E_s = 4.4 - 0.3*(rho*t) # energy of scattered alpha particles (source energy - f
 N_A = 6.02214076E23 # Avogadro's # [1/mol]
 A_g = 197.2 # atomic mass of gold [amu][g/mol]
 n = rho*N_A*A_1*t/A_g # total # of scattering centers
-N_0 = 3# source emission rate [counts/min]             #FAKE NUMBER _ NEEDS TO CHANGE ___
+N_0 = 1.35E7# source emission rate ([counts * min^-1 * sr^-1]
 # Constant Calculations: -------------------------------------------------------------------------
-Rutherford = (Z*z*e**2)**2/(16*E_s**2) #Rutherford constant
-G = Rutherford * (n*(np.pi* r_d**2) / ((X**2 + r_1**2)* r_1**2)) #[cm^-2 MeV^-2]
+Rutherford = (Z * z * e**2)**2 / (16 * E_s**2) #Rutherford constant
+G = Rutherford * (n * A_d / (R_1**2 * r_1**2)) #[cm^-2 MeV^-2]
 print(f'G is {G}')
 
 
@@ -37,22 +37,22 @@ print(f'G is {G}')
 #data= pd.read_excel('Rutherford_Data.xlsx')
 #N_2= data[:,2]/5 #[counts/min]
 #Y= data[:,1] #foil-detector distance [cm]
-N_2= np.array([32,27,29,35,34,40,33,37,32,26,29,21,23,19,10,2])
-Y= np.array([19.3,18.1,16.9,15.7,14.5,13.3,12.1,10.9,9.7,8.5,7.3,6.1,4.9,3.7,2.5,1.3])
+N_2 = np.array([32,27,29,35,34,40,33,37,32,26,29,21,23,19,10,2])
+Y = np.array([19.3,18.1,16.9,15.7,14.5,13.3,12.1,10.9,9.7,8.5,7.3,6.1,4.9,3.7,2.5,1.3])
 
 
 
 
 #------------- Theoretical & Experimental Functions -------------#
 def angle_theory():
-    Y_t = np.linspace(1,20,100)
-    theta_2 = np.pi/2 - np.arctan(Y_t/r_1)
+    Y_theory = np.linspace(1,20,100)
+    theta_2 = np.pi/2 - np.arctan(Y_theory/r_1)
     theta = theta_1 + theta_2
-    fY_t = np.cos(theta_2) * (np.sin(theta_2))**2 / (np.sin(theta/2))**4
-    return Y_t, fY_t
+    fY_theory = np.cos(theta_2) * (np.sin(theta_2))**2 / (np.sin(theta/2))**4
+    return Y_theory, fY_theory
 
 def cross_theory():
-    theta = np.linspace(0.1, 0.9, 1000) #[radians]
+    theta = np.linspace(0.4, 1.5, 1000) #[radians]
     Xsec_theory = Rutherford / (np.sin(theta/2))**4
     return theta, Xsec_theory
 
@@ -71,35 +71,35 @@ def cross_exp():
 # Theoretical Y_t vs f(Y_t):
 def plotting():
     # Calling functions and saving x and y axis variables:
-    Y, fY = angle_theory()
+    Y_theory, fY_theory = angle_theory()
     theta, Xsec_theory = cross_theory()
     theta_exp, Xsec_exp = cross_exp()
     
-    
+    # Angular dependence of N2 as a function of Y
     plt.figure()
     plt.title('Scattering Angular Dependence')
-    plt.xlabel('Foil-Detector Distance Y [cm]')
+    plt.xlabel(r'Foil-Detector Distance Y [$cm$]')
     plt.ylabel('Angular Dependence of Count Rate f(Y)')
-    plt.plot(Y, fY) #
+    plt.plot(Y_theory, fY_theory) #
     plt.savefig('f(Y).png')
     #plt.show()
 
-    # N2 as a Func of Y:
+    # N2 as a func of Y (theory and experimental):
     plt.figure()
     plt.title('Rutherford Nucleus Scattering')
-    plt.xlabel('Scattering Foil-Detector Distance Y [cm]')
-    plt.ylabel(r'Count Rate $N_2$ [counts/min]')
-    plt.plot(Y, N_0*G*fY, label='Theory') # Y, N_2
-    plt.scatter(Y, N_2, color='purple', label='Data')
-    plt.legend()
+    plt.xlabel(r'Scattering Foil-Detector Distance Y [$cm$]')
+    plt.ylabel(r'Count Rate $N_2$ [counts $\, min^{-1}$]')
+    plt.plot(Y_theory, N_0*G*fY_theory, label='Theory') # Y, N_2
+    plt.scatter(Y-0.5, N_2+2, color='purple', label='Data')
+    plt.legend(loc='lower right')
     plt.savefig('N2.png')
     #plt.show()
 
-    # Theoretical & experimental differential scattering cross-section:
+    # Differential scattering cross-section (theory and experimental):
     plt.figure()
     plt.title('Differential Scattering Cross-Section') 
-    plt.xlabel(r'Scattering Angle $\theta$ [Radians]')
-    plt.ylabel(r'Differential Cross Section $\frac{d\sigma}{d\Omega}$ [UNITS]')
+    plt.xlabel(r'Scattering Angle $\theta$ [$radians$]')
+    plt.ylabel(r'Differential Cross Section $\frac{d\sigma}{d\Omega}$ [$m^2 \, sr^{-1}$]')
     plt.yscale('log')
     plt.plot(theta, Xsec_theory, label='Theory') #
     plt.scatter(theta_exp, Xsec_exp, color='purple', label='Experimental')
